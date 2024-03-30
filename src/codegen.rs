dd46243bb73d2da1f909b12b58a57182aed33845
Jack T <jack@jackt.space>
2024-03-29 23:29:59 -0700

    Abuse anyhow to the max?

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,44 +1,49 @@
 use crate::lockfile::LockFile;
+use anyhow::Context;
 use std::path::Path;
 
-pub fn generate_lua(lockfile: &LockFile, strip_dir: &str) -> String {
+pub fn generate_lua(lockfile: &LockFile, strip_dir: &str) -> anyhow::Result<String> {
     let table = lockfile
         .entries
         .iter()
         .map(|(file_path, file_entry)| {
             let file_stem = Path::new(file_path)
                 .to_str()
-                .unwrap()
+                .context("Failed to convert path to string")?
                 .strip_prefix(strip_dir)
-                .unwrap();
-            format!(
+                .context("Failed to strip directory prefix")?;
+            Ok(format!(
                 "\t[\"{}\"] = \"rbxassetid://{}\"",
                 file_stem, file_entry.asset_id
-            )
+            ))
         })
-        .collect::<Vec<String>>()
+        .collect::<Result<Vec<String>, anyhow::Error>>()?
         .join(",\n");
 
-    format!("return {{\n{}\n}}", table)
+    Ok(format!("return {{\n{}\n}}", table))
 }
 
-pub fn generate_ts(lockfile: &LockFile, strip_dir: &str, output_dir: &str) -> String {
+pub fn generate_ts(
+    lockfile: &LockFile,
+    strip_dir: &str,
+    output_dir: &str,
+) -> anyhow::Result<String> {
     let interface = lockfile
         .entries
         .keys()
         .map(|file_path| {
             let file_stem = Path::new(file_path)
                 .to_str()
-                .unwrap()
+                .context("Failed to convert path to string")?
                 .strip_prefix(strip_dir)
-                .unwrap();
-            format!("\t\"{}\": string", file_stem)
+                .context("Failed to strip directory prefix")?;
+            Ok(format!("\t\"{}\": string", file_stem))
         })
-        .collect::<Vec<String>>()
+        .collect::<Result<Vec<String>, anyhow::Error>>()?
         .join(",\n");
 
-    format!(
+    Ok(format!(
         "declare const {}: {{\n{}\n}}\nexport = {}",
         output_dir, interface, output_dir
-    )
+    ))
 }
