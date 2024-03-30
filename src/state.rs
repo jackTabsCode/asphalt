8b134dd65261f3142df48bc3161497de3deda5ed
Jack T <jack@jackt.space>
2024-03-30 00:41:56 -0700

    More anyhow abuse

--- src/state.rs
+++ src/state.rs
@@ -1,10 +1,11 @@
-use std::path::PathBuf;
+use std::{env, path::PathBuf};
 
 use crate::{
     args::Args,
     config::{Config, CreatorType},
     LockFile,
 };
+use anyhow::Context;
 use rbxcloud::rbx::v1::assets::{AssetCreator, AssetGroupCreator, AssetUserCreator};
 use resvg::usvg::fontdb::Database;
 use tokio::fs::{create_dir_all, read_to_string};
@@ -17,6 +18,14 @@ fn add_trailing_slash(path: &str) -> String {
     path.to_string()
 }
 
+fn get_api_key(arg_key: Option<String>) -> anyhow::Result<String> {
+    let env_key = env::var("ASPHALT_API_KEY");
+
+    match arg_key {
+        Some(key) => Ok(key),
+        None => env_key.context("No API key provided"),
+    }
+}
 pub struct State {
     pub asset_dir: PathBuf,
     pub write_dir: PathBuf,
@@ -34,10 +43,8 @@ pub struct State {
 }
 
 impl State {
-    pub async fn new(args: Args, config: &Config) -> Self {
-        let api_key: String = args
-            .api_key
-            .unwrap_or_else(|| std::env::var("ASPHALT_API_KEY").expect("No API key provided"));
+    pub async fn new(args: Args, config: &Config) -> anyhow::Result<Self> {
+        let api_key = get_api_key(args.api_key)?;
 
         let creator: AssetCreator = match config.creator.creator_type {
             CreatorType::User => AssetCreator::User(AssetUserCreator {
@@ -51,9 +58,9 @@ impl State {
         let asset_dir = add_trailing_slash(&config.asset_dir);
         let asset_dir = PathBuf::from(asset_dir);
 
-        create_dir_all(&config.write_dir)
+        let _ = create_dir_all(&config.write_dir)
             .await
-            .expect("Failed to create write directory");
+            .context("Failed to create write directory");
         let write_dir = add_trailing_slash(&config.write_dir);
         let write_dir = PathBuf::from(write_dir);
 
@@ -83,7 +90,7 @@ impl State {
 
         let new_lockfile: LockFile = Default::default();
 
-        Self {
+        Ok(Self {
             asset_dir,
             write_dir,
             api_key,
@@ -94,6 +101,6 @@ impl State {
             font_db,
             existing_lockfile,
             new_lockfile,
-        }
+        })
     }
 }
